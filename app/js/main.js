d6e147ed72d54f1938707be120fbf2f9a1b44359
webAqemu <notych2812@gmail.com>
2020-05-21 11:29:02 +0300

    added reasons section and start making slider section

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,15 @@ $(function () {
 		$('.lnr-chevron-down').toggleClass('active')
 	});
 
+	$(".description__stars").rateYo({
+		rating: 4.5,
+		starWidth: "17px",
+		readOnly: true,
+	});
+
+	/* 	$('.products-slider__inner').slick({
+			slidesToShow: 1,
+			slidesToScroll: 1,
+		}); */
+
 });
