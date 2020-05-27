23ed1f150009b86f5a0609f01937ba16f1e87fc3
webAqemu <notych2812@gmail.com>
2020-05-27 14:25:58 +0300

    fixed sliders and add two sections: feed, stats

--- app/js/main.js
+++ app/js/main.js
@@ -5,18 +5,29 @@ $(function () {
 		$('.lnr-chevron-down').toggleClass('active')
 	});
 
-	$(".description__stars").rateYo({
+	$(".weekly__stars").rateYo({
+		rating: 4.5,
+		starWidth: "17px",
+		readOnly: true,
+	});
+
+	$(".product__stars").rateYo({
 		rating: 4.5,
 		starWidth: "15px",
 		readOnly: true,
 	});
 
-	$('.products-slider__inner').slick({
+	$('.weekly__inner').slick({
 		slidesToShow: 1,
 		slidesToScroll: 1,
 	});
 
-	var mixer = mixitup('.products__box');
+	var mixer = mixitup('.newest__box');
 
+	$('.feed__inner').slick({
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		variableWidth: true,
+	});
 
 });
