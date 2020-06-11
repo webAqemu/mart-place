e06924fdc886e3f00f1dad248c4f217b6e26b8a1
webAqemu <notych2812@gmail.com>
2020-06-11 23:33:55 +0300

    added adaptive and fixed bugs

--- app/js/main.js
+++ app/js/main.js
@@ -22,12 +22,45 @@ $(function () {
 		slidesToScroll: 1,
 	});
 
-	var mixer = mixitup('.newest__box');
-
 	$('.feed__inner').slick({
 		slidesToShow: 3,
 		slidesToScroll: 3,
 		variableWidth: true,
+		responsive: [
+			{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				}
+			},
+			{
+				breakpoint: 700,
+				settings: {
+					slidesToScroll: 1,
+				}
+			},
+
+		]
 	});
+
+	$('.header__burger').on('click', function () {
+		$('.header__menu-inner').slideToggle();
+	});
+
+	$('.header__item').on('click', function () {
+		$(this).children('.header__dropdown').toggleClass('active');
+	});
+
+	$('.pages__col').on('click', function () {
+		$(this).children('.pages__list').toggleClass('active');
+	});
+
+	$('.header__btn--top').on('click', function () {
+		$('.header__box--top').toggleClass('active');
+	});
+
+	var mixer = mixitup('.newest__box');
+
 
 });
